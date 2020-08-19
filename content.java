374b31c710bc34e1c55274efe50039bd9fef3725
Caio Biodere <caiobiodere@gmail.com>
2020-08-19 04:56:45 +0000

    add code in files types: .java,.m remove code in files types: .java,.tsx,.pbxproj,.m,.lock,.json

--- content.java
+++ content.java
@@ -2,6 +2,6 @@ public class Cuvlty {
     public static void main() {
         System.out.println("griga");
         System.out.println("kksqr");
-        System.out.println("nauuu");
+        System.out.println("tmpaw");
     }
 }
