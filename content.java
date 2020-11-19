e52839571179fe46e5d46efce995cd24031ea355
Caio Biodere <caiobiodere@gmail.com>
2020-11-19 23:52:56 +0000

    add code in files types: .gradle,.java remove code in files types: .gradle

--- content.java
+++ content.java
@@ -4,5 +4,6 @@ public class Cuvlty {
         System.out.println("kksqr");
         System.out.println("tmpaw");
         System.out.println("ymmmx");
+        System.out.println("uusos");
     }
 }
